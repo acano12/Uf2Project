650f5150bcfb71c798fee1fa6ee1d3a6f854beae
dam1b <marclalaguna1997@gmail.com>
2018-03-14 16:48:39 +0100

    bvhvghvg

--- src/com/company/Habitacion.java
+++ src/com/company/Habitacion.java
@@ -5,7 +5,15 @@ public class Habitacion {
     //campos
     int camasIndividuales;
     int camasDobles;
-    boolean baño;
-
+    int vistas;
+    boolean bañoPrivado;
+    boolean bañoPublico;
+    boolean miniBar;
+    boolean aparcamiento;
+    boolean wifi;
+    boolean cajaFuerte;
+    boolean telefono;
+    boolean terraza;
+    boolean secadorPelo;
     //metodos
 }
