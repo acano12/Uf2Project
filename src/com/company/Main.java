38cb177b4198ecfd58544c4a6b177b19c399878b
dam1b <marclalaguna1997@gmail.com>
2018-04-09 19:39:17 +0200

    Merge branch 'master' of /home/dam1b/IdeaProjects/Uf2Project with conflicts.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,42 @@
+package /home/dam1b/IdeaProjects/practica12/practica12.imlcom.company;
 
-package com.company;
-
-import com.company.view.PantallaClientes;
+import com.company.model.Habitacion;
+import com.company.manager.ManagerReserva;
+import com.company.view.PantallaListarReservas;
+import com.company.view.PantallaReserva;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        //CLIENTES
-        PantallaClientes pantallaClientes = new PantallaClientes();
-        pantallaClientes.mostrar();
+        Habitacion habitacionDoble = new Habitacion();
 
-//        Habitacion habitacionDoble = new Habitacion();
-//
-//        Habitacion habitacionExecutive = new Habitacion();
-//
-//        Habitacion suite = new Habitacion();
-//
-//        //HABITACION-INDIVIDUAL//
-//
-//        Habitacion habitacionIndividual_Blue = new Habitacion();
-//        habitacionIndividual_Blue.asignarNumerodeCamasIndividuales(3);
-//        habitacionIndividual_Blue.vistas=true;
-//        habitacionIndividual_Blue.bañoPublico=true;
-//
-//        Habitacion habitacionIndividual_RedLava = new Habitacion();
-//        habitacionIndividual_RedLava.asignarNumerodeCamasIndividuales(2);
-//        habitacionIndividual_RedLava.vistas=true;
-//        habitacionIndividual_RedLava.bañoPrivado=true;
-//        habitacionIndividual_RedLava.wifi=true;
+        Habitacion habitacionExecutive = new Habitacion();
+
+        Habitacion suite = new Habitacion();
+
+        //HABITACION-INDIVIDUAL//
+
+        Habitacion habitacionIndividual_Blue = new Habitacion();
+        habitacionIndividual_Blue.asignarNumerodeCamasIndividuales(3);
+        habitacionIndividual_Blue.vistas=true;
+        habitacionIndividual_Blue.bañoPublico=true;
+
+        Habitacion habitacionIndividual_RedLava = new Habitacion();
+        habitacionIndividual_RedLava.asignarNumerodeCamasIndividuales(2);
+        habitacionIndividual_RedLava.vistas=true;
+        habitacionIndividual_RedLava.bañoPrivado=true;
+        habitacionIndividual_RedLava.wifi=true;
+
+        ManagerReserva managerReserva = new ManagerReserva();
 
 
+        PantallaReserva pantallaReserva = new PantallaReserva();
+        pantallaReserva.mostrar(managerReserva);
+        //pantallaReserva.iniciar(managerReserva);
+
+        PantallaListarReservas pantallaListarReservas = new PantallaListarReservas();
+        pantallaListarReservas.mostrar(managerReserva);
 
     }
 }
