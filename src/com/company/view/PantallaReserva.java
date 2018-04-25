3f1bce95064abb00dbb9e6b47f8ead6eca7941ea
usman munawar <mmunawar@elpuig.xeill.net>
2018-04-25 17:14:31 +0200

    FECHAS

--- src/com/company/view/PantallaReserva.java
+++ src/com/company/view/PantallaReserva.java
@@ -13,31 +13,38 @@ public class PantallaReserva {
         Scanner scanner = new Scanner(System.in);
         Mensaje mensaje = new Mensaje();
         EditText editText = new EditText();
-
-        String f1Inicio;
-
-
+        String fecha1 = editText.read("Escoja fecha de entrada");
         ManagerReserva managerReserva = new ManagerReserva();
-//        managerReserva.comprobar(fecha);
-//
-        // ManagerReserva.getHabitacionesLibres(any, dia, mes);
-
-        int validar = ManagerReserva.comprobar(100,1,1);
-
-
-        if (validar == 1) {
-            mensaje.showError("LO SENTIMOS, RESERVA FECHA NO DISPONIBLE");
-
-            PantallaReserva pantallaReserva = new PantallaReserva();
-            pantallaReserva.reservar();
-            System.out.println(" ");
-        }
-           mensaje.showOk("ENHORABUENA, FECHA ACCESIBLE");
-
-        System.out.println(" ");
-            PantallaHabitacion pantallaHabitacion = new PantallaHabitacion();
-            pantallaHabitacion.habitaciones();
-
+        managerReserva.fecha1(fecha1);
+        String fecha2 = editText.read("Escoja fecha de salida");
+        managerReserva.fecha2(fecha2);
 
     }
 }
+//        String f1Inicio;
+//
+//
+//        ManagerReserva managerReserva = new ManagerReserva();
+////        managerReserva.comprobar(fecha);
+////
+//        // ManagerReserva.getHabitacionesLibres(any, dia, mes);
+//
+//        int validar = ManagerReserva.comprobar(100,1,1);
+//
+//
+//        if (validar == 1) {
+//            mensaje.showError("LO SENTIMOS, RESERVA FECHA NO DISPONIBLE");
+//
+//            PantallaReserva pantallaReserva = new PantallaReserva();
+//            pantallaReserva.reservar();
+//            System.out.println(" ");
+//        }
+//           mensaje.showOk("ENHORABUENA, FECHA ACCESIBLE");
+//
+//        System.out.println(" ");
+//            PantallaHabitacion pantallaHabitacion = new PantallaHabitacion();
+//            pantallaHabitacion.habitaciones();
+//
+//
+//    }
+//}
